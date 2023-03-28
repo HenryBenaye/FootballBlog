df14b287cd6dd09a84a04dbfeb7b49a03a66bca1
Henry Benaye <henry.be@outlook.com>
2023-03-28 12:13:22 +0200

    Delete like update

---
+++ VoetbalBlog/VoetbalProject/app/Models/Like.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Like extends Model
+{
+    use HasFactory;
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+}
