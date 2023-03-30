2f275e00d6d678b9045aaf252ca9d5d9b131bf0a
Henry Benaye <henry.be@outlook.com>
2023-03-30 18:40:12 +0200

    Thread store, table

--- VoetbalBlog/VoetbalProject/resources/views/dashboard.blade.php
+++ VoetbalBlog/VoetbalProject/resources/views/dashboard.blade.php
@@ -104,7 +104,7 @@
         </div>
     </div>
 
-    <!-- newTweet modal -->
+    <!-- newComment modal -->
     <div id="newComment-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
         <div class="relative w-full h-full max-w-md md:h-auto">
             <!-- Modal content -->
